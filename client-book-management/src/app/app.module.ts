55abab692bbe3ecf3a47c167f6ae4901af5e9990
Amauri Klamas <jrklamas@hotmail.com>
2020-05-04 20:23:09 -0300

    sprint 3 tela login

--- client-book-management/src/app/app.module.ts
+++ client-book-management/src/app/app.module.ts
@@ -16,14 +16,43 @@ import { AdminTemplateComponent } from './components/template/admin-template/adm
 import { NotFoundComponent } from './components/error/not-found/not-found.component';
 import { UnathorizedComponent } from './components/error/unathorized/unathorized.component';
 
-import {
-  MatButtonModule, MatCardModule,
-  MatInputModule, MatListModule,
-  MatToolbarModule, MatSelectModule,
-  MatFormFieldModule, MatTableModule,
-  MatPaginatorModule, MatSortModule,
-  MatProgressBarModule, MatIconModule
-} from '@angular/material';
+// material.module.ts
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
@@ -57,7 +86,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms'
     MatFormFieldModule,
     MatTableModule,
     MatPaginatorModule,
-    MaatSortModule,
+    MatSortModule,
     MatProgressBarModule,
     MatIconModule,
     BrowserAnimationsModule,
